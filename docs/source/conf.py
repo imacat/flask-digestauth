6c7f7e8c8e782eb5efa1e091542dd5e283ddc75a
依瑪貓 <imacat@mail.imacat.idv.tw>
2022-12-06 20:02:53 +0800

    Added the comment to the master_doc setting in conf.py.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,4 +55,5 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# For readthedocs.io to work properly.
 master_doc = 'index'
